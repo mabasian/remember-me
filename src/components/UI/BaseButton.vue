7df07d19802517a9f582635996816dd9dd4dcb5c
mabasian <54101509+mabasian@users.noreply.github.com>
2021-11-13 22:23:59 +0100

    Add files via upload

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,50 @@
+<template>
+  <button :class="mode">
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['mode'],
+};
+</script>
+
+<style scoped>
+button {
+  width: 80px;
+  height: 30px;
+  font-size: 0.8em;
+  background: none;
+  border: none;
+  color: #148110;
+  position: relative;
+  cursor: pointer;
+}
+button::before,
+button::after {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: -5%;
+  width: 100%;
+  height: 100%;
+  background: #00000000;
+  backdrop-filter: blur(70px);
+  border-radius: 10px;
+  z-index: -2;
+}
+button::after {
+  width: 110%;
+  height: 90%;
+  top: 25%;
+  left: -5%;
+  background: #148110;
+  z-index: -3;
+  transform: scaleX(0.5);
+  transition: all 0.6s;
+}
+button:hover::after {
+  transform: scaleX(1.01) translateY(-15px);
+}
+</style>
---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,73 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+
+export default {
+  components: {
+    StoredResources,
+    AddResource,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org',
+        },
+      ],
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources,
+      addResource: this.addResource,
+      deleteResource: this.removeResource,
+    };
+  },
+
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+    addResource(title, description, url) {
+      const newResource = {
+        id: new Date().toISOString(),
+        title: title,
+        description: description,
+        link: url,
+      };
+      this.storedResources.unshift(newResource);
+      this.selectedTab = 'stored-resources';
+    },
+    removeResource(resId) {
+      const resIndex = this.storedResources.findIndex(
+        (res) => res.id === resId
+      );
+      this.storedResources.splice(resIndex, 1);
+    },
+  },
+};
+</script>
